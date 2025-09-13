ad767de2e8bb258edb3c7f680a3728e397d88ea1
Olivier Hoareau <oha@greenberets.io>
2025-09-13 15:19:27 +0200

    fix: fix

--- modules/events-api-regional/cloudwatch.tf
+++ modules/events-api-regional/cloudwatch.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "log" {
   count             = local.log_enabled ? 1 : 0
-  name              = "/aws/appsync/apis/${var.name}"
+  name              = "/aws/appsync/apis/${aws_appsync_api.regional.api_id}"
   retention_in_days = 14
 }
