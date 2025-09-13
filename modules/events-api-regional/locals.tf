03f0b2d306332714350344b455072908a74d8952
Olivier Hoareau <oha@greenberets.io>
2025-09-13 15:07:00 +0200

    fix: fix

--- modules/events-api-regional/locals.tf
+++ modules/events-api-regional/locals.tf
@@ -1,5 +1,5 @@
 locals {
   custom_domain       = var.dns != null && var.dns_zone != null
   authentication_type = var.user_pool_id != null ? "AMAZON_COGNITO_USER_POOLS" : (var.authorizer_lambda_arn != null ? "AWS_LAMBDA" : "API_KEY")
-  log_enabled         = null == var.log_level
+  log_enabled         = null != var.log_level
 }
